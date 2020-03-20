b5440fd770b111f4059b1d991ef3882f25d4dc1e
Karley Petracca <karleypetracca@gmail.com>
2020-03-20 10:22:49 -0400

    Finished portfolio section and added 100vh sections

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+
+// applies smoother scrolling for anchor same-page links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// animated arrow
+
